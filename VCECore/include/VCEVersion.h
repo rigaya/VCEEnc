2370dbe551a7a7a23397a682e4768addf9c52daf
rigaya <rigaya34589@live.jp>
2014-02-17 13:20:31 +0900

    QSVのままになっていたのを修正

--- VCECore/include/VCEVersion.h
+++ VCECore/include/VCEVersion.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-#define VER_FILEVERSION          0,0,2,5
-#define VER_STR_FILEVERSION      "0.02"
+#define VER_FILEVERSION          0,0,3,0
+#define VER_STR_FILEVERSION      "0.03"
 
-#define VCE_AMD_APP_SDK          "2.8"
+#define VCE_AMD_APP_SDK          "2.8.1"
