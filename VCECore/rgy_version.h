006e591cac9e9a704815e28446e952b81b56fa0a
rigaya <rigaya34589@live.jp>
2018-11-19 23:18:36 +0900

    入力としてffmpegのプロトコルを使用する場合に、正常に処理できなくなっていたのを修正。

--- VCECore/rgy_version.h
+++ VCECore/rgy_version.h
@@ -29,9 +29,9 @@
 #ifndef __RGY_VERSION_H__
 #define __RGY_VERSION_H__
 
-#define VER_FILEVERSION             0,4,0,0
-#define VER_STR_FILEVERSION          "4.00"
-#define VER_STR_FILEVERSION_TCHAR _T("4.00")
+#define VER_FILEVERSION             0,4,1,0
+#define VER_STR_FILEVERSION          "4.01"
+#define VER_STR_FILEVERSION_TCHAR _T("4.01")
 
 #define ENABLE_OPENCL 1
 
