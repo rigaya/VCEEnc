feef845f6e6e4979f9373a765f889be2f66744ca
rigaya <rigaya34589@live.jp>
2020-05-31 11:07:32 +0900

    エンコーダの差を吸収するフラグを整理。

--- VCECore/rgy_version.h
+++ VCECore/rgy_version.h
@@ -56,6 +56,9 @@ const char *get_encoder_version();
 #define ENABLE_CPP_REGEX 1
 #define ENABLE_DTL 1
 
+#define ENABLE_DHDR10_INFO 0
+#define ENABLE_KEYFRAME_INSERT 0
+#define ENABLE_AUTO_PICSTRUCT 0
 
 #define GPU_VENDOR "AMD"
 #define DECODER_NAME  "vce"
