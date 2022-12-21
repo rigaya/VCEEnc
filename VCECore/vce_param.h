d1f392c44922bf4241805320700e9290252c7f59
rigaya <rigaya34589@live.jp>
2022-12-21 23:01:24 +0900

    --cqpの設定のHEVCでの修正。( #70 )

--- VCECore/vce_param.h
+++ VCECore/vce_param.h
@@ -726,7 +726,7 @@ static const wchar_t *AMF_PARAM_GOP_SIZE(RGY_CODEC codec) {
 static const wchar_t *AMF_PARAM_QP_I(RGY_CODEC codec) {
     switch (codec) {
     case RGY_CODEC_AV1:  return AMF_VIDEO_ENCODER_AV1_Q_INDEX_INTRA;
-    case RGY_CODEC_HEVC: return AMF_VIDEO_ENCODER_HEVC_MIN_QP_I;
+    case RGY_CODEC_HEVC: return AMF_VIDEO_ENCODER_HEVC_QP_I;
     case RGY_CODEC_H264:
     default:             return AMF_VIDEO_ENCODER_QP_I;
     }
@@ -734,7 +734,7 @@ static const wchar_t *AMF_PARAM_QP_I(RGY_CODEC codec) {
 static const wchar_t *AMF_PARAM_QP_P(RGY_CODEC codec) {
     switch (codec) {
     case RGY_CODEC_AV1:  return AMF_VIDEO_ENCODER_AV1_Q_INDEX_INTER;
-    case RGY_CODEC_HEVC: return AMF_VIDEO_ENCODER_HEVC_MIN_QP_P;
+    case RGY_CODEC_HEVC: return AMF_VIDEO_ENCODER_HEVC_QP_P;
     case RGY_CODEC_H264:
     default:             return AMF_VIDEO_ENCODER_QP_P;
     }
