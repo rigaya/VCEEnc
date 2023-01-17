ecfcf7a62701be03bc431928e162cec8c235be24
rigaya <rigaya34589@live.jp>
2023-01-17 18:55:28 +0900

    宣言が長すぎるので改行。

--- VCECore/vce_core.h
+++ VCECore/vce_core.h
@@ -110,7 +110,8 @@ protected:
     virtual RGY_ERR initPerfMonitor(VCEParam *prm);
     virtual RGY_ERR initDecoder(VCEParam *prm);
     virtual RGY_ERR initFilters(VCEParam *prm);
-    virtual std::vector<VppType> InitFiltersCreateVppList(const VCEParam *inputParam, const bool cspConvRequired, const bool cropRequired, const RGY_VPP_RESIZE_TYPE resizeRequired);
+    virtual std::vector<VppType> InitFiltersCreateVppList(const VCEParam *inputParam,
+        const bool cspConvRequired, const bool cropRequired, const RGY_VPP_RESIZE_TYPE resizeRequired);
     virtual RGY_ERR AddFilterOpenCL(std::vector<std::unique_ptr<RGYFilter>>&clfilters,
         RGYFrameInfo & inputFrame, const VppType vppType, const VCEParam *prm, const sInputCrop * crop, const std::pair<int, int> resize);
     virtual std::tuple<RGY_ERR, std::unique_ptr<AMFFilter>> AddFilterAMF(
