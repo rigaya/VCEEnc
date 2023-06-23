101a1b3dcd16e0bf951f2897187396ba64be3e09
rigaya <rigaya34589@live.jp>
2023-06-24 08:49:05 +0900

    入力のcolorprimの指定がない場合に、--vpp-colorspaceのhdr2sdrが正常に動作しない問題を修正。

--- VCECore/rgy_version.h
+++ VCECore/rgy_version.h
@@ -29,9 +29,9 @@
 #ifndef __RGY_VERSION_H__
 #define __RGY_VERSION_H__
 
-#define VER_FILEVERSION             0,8,15,0
-#define VER_STR_FILEVERSION          "8.15"
-#define VER_STR_FILEVERSION_TCHAR _T("8.15")
+#define VER_FILEVERSION             0,8,16,0
+#define VER_STR_FILEVERSION          "8.16"
+#define VER_STR_FILEVERSION_TCHAR _T("8.16")
 
 
 #ifdef _M_IX86
