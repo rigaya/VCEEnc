e17a06ac406eb84fd5cb11106624b5647d19d826
rigaya <rigaya34589@live.jp>
2024-05-11 08:49:37 +0900

    ENABLE_LIBASS_SUBBURNが機能していなかったのを修正。

--- VCECore/rgy_version.h
+++ VCECore/rgy_version.h
@@ -69,6 +69,7 @@ const char *get_encoder_version();
 #define VULKAN_DEFAULT_DEVICE_ONLY 0
 #define ENABLE_CPP_REGEX 1
 #define ENABLE_DTL 1
+#define ENABLE_LIBASS_SUBBURN 1
 
 #define AV_CHANNEL_LAYOUT_STRUCT_AVAIL 1
 #define AV_FRAME_DURATION_AVAIL 1
