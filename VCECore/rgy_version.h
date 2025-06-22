18e6d1be21c018bed3b23cc909f6ded5c2bf08a1
rigaya <rigaya34589@live.jp>
2025-06-22 11:20:16 +0900

    raw出力時に処理が止まってしまう場合があったのを修正。

--- VCECore/rgy_version.h
+++ VCECore/rgy_version.h
@@ -29,9 +29,9 @@
 #ifndef __RGY_VERSION_H__
 #define __RGY_VERSION_H__
 
-#define VER_FILEVERSION             0,8,36,0
-#define VER_STR_FILEVERSION          "8.36"
-#define VER_STR_FILEVERSION_TCHAR _T("8.36")
+#define VER_FILEVERSION             0,8,37,0
+#define VER_STR_FILEVERSION          "8.37"
+#define VER_STR_FILEVERSION_TCHAR _T("8.37")
 
 
 #ifdef _M_IX86
