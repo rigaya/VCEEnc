ca41806187f7d10851b45ee22a9f7ef06721e059
rigaya <rigaya34589@live.jp>
2026-06-02 08:48:02 +0900

    --vpp-dehaloのユーザー提供実装を取り込み ほかを取り込み
    Source commits:
    - 83cef381 --vpp-dehaloのユーザー提供実装を取り込み
    - 3e778944 --vpp-dehaloのQSVEnc配線を追加
    - 30427888 --vpp-dehaloのevent chainとchroma copyを修正
    - 14673d6a ドキュメント更新。

--- VCECore/vce_core.cpp
+++ VCECore/vce_core.cpp
@@ -88,6 +88,7 @@
 #include "rgy_filter_deblock.h"
 #include "rgy_filter_deflicker.h"
 #include "rgy_filter_colorfix.h"
+#include "rgy_filter_dehalo.h"
 #include "rgy_filter_edgelevel.h"
 #include "rgy_filter_msharpen.h"
 #include "rgy_filter_warpsharp.h"
@@ -1435,6 +1436,7 @@ std::vector<VppType> VCECore::InitFiltersCreateVppList(const VCEParam *inputPara
     if (inputParam->vpp.deblock.enable)    filterPipeline.push_back(VppType::CL_DEBLOCK);
     if (inputParam->vpp.deflicker.enable)  filterPipeline.push_back(VppType::CL_DEFLICKER);
     if (inputParam->vpp.colorfix.enable)   filterPipeline.push_back(VppType::CL_COLORFIX);
+    if (inputParam->vpp.dehalo.enable)     filterPipeline.push_back(VppType::CL_DEHALO);
     if (inputParam->vpp.edgelevel.enable)  filterPipeline.push_back(VppType::CL_EDGELEVEL);
     if (inputParam->vpp.msharpen.enable)   filterPipeline.push_back(VppType::CL_MSHARPEN);
     if (inputParam->vpp.warpsharp.enable)  filterPipeline.push_back(VppType::CL_WARPSHARP);
@@ -2633,6 +2635,29 @@ RGY_ERR VCECore::AddFilterOpenCL(std::vector<std::unique_ptr<RGYFilter>>&clfilte
         unique_ptr<RGYFilter> filter(new RGYFilterEdgelevel(m_dev->cl()));
         shared_ptr<RGYFilterParamEdgelevel> param(new RGYFilterParamEdgelevel());
         param->edgelevel = inputParam->vpp.edgelevel;
+        param->frameIn = inputFrame;
+        param->frameOut = inputFrame;
+        param->baseFps = m_encFps;
+        param->bOutOverwrite = false;
+        auto sts = filter->init(param, m_pLog);
+        if (sts != RGY_ERR_NONE) {
+            return sts;
+        }
+        //フィルタチェーンに追加
+        clfilters.push_back(std::move(filter));
+        //パラメータ情報を更新
+        m_pLastFilterParam = std::dynamic_pointer_cast<RGYFilterParam>(param);
+        //入力フレーム情報を更新
+        inputFrame = param->frameOut;
+        m_encFps = param->baseFps;
+        return RGY_ERR_NONE;
+    }
+    //dehalo
+    if (vppType == VppType::CL_DEHALO) {
+        amf::AMFContext::AMFOpenCLLocker locker(m_dev->context());
+        unique_ptr<RGYFilter> filter(new RGYFilterDehalo(m_dev->cl()));
+        shared_ptr<RGYFilterParamDehalo> param(new RGYFilterParamDehalo());
+        param->dehalo = inputParam->vpp.dehalo;
         param->frameIn = inputFrame;
         param->frameOut = inputFrame;
         param->baseFps = m_encFps;
